ecba570b018d36be49bfaa14b7aa79fb1f18f1b8
Justin <jstnwrds55@gmail.com>
2020-04-09 19:28:51 -0600

    implemented binary search after selection sort

--- Selection Sort/selection_script.js
+++ Selection Sort/selection_script.js
@@ -2,17 +2,19 @@
 
 // set number of elements based on window width
 width = window.innerWidth;
-numElements = Math.floor(width / 46);
+numElements = Math.floor(width / 47);
 let numbers = []
 for (i = 0; i < numElements; i++) {
   numbers.push(Math.floor(Math.random() * 99) + 1);
 }
+numbers[0] = 1;
 
 // sort variables
 let speed = 100;
 let sortStep = 0;
 let running = false;
 
+// slider stuff
 slider = document.getElementById("myRange");
 speed = 200 - slider.value
 slider.oninput = function() {
@@ -21,21 +23,21 @@ slider.oninput = function() {
 };
 
 let startButton = document.getElementById("startButton")
-
 // start/stop sort on button press
 startButton.onclick = function() {
   if (startButton.innerText == "Start Sort" || startButton.innerText == "Resume Sort") {
     running = true;
     startButton.innerText = "Pause Sort";
+    document.getElementById("result").innerText = "Sorting...";
     sortSingleStep(sortStep);
     sortStep++;
   } else if (startButton.innerText == "Pause Sort") {
     running = false;
     startButton.innerText = "Pausing...";
+  } else if (startButton.innerText == "Binary Search") {
+    startBinarySearch();
   }
  };
-
- document
 
 // element for copying
 var element = document.getElementById("placeholder");
@@ -45,16 +47,16 @@ for (index = 0; index < numbers.length; index++) {
 	var newElement = element.cloneNode(true); // copy
   newElement.innerHTML = numbers[index]; // number
   newElement.id = index; // index as id
-  console.log((Math.log(numbers[index])).toString());
   newElement.style.paddingTop = (Math.log(numbers[index]) * 4).toString() + "%";
 	document.getElementById("elements").appendChild(newElement);
 }
 
 var button = document.getElementById("stepButton");
 button.onclick = function () {
-	sortSingleStep(sortStep);
-  sortStep++;
-  button.isenable
+  if (sortStep <= numbers.length + 5) {
+    sortSingleStep(sortStep);
+    sortStep++;
+  } 
 };
 
 // single step of sort
@@ -73,7 +75,6 @@ function sortSingleStep(startIndex) {
         
           // set comparing style
           document.getElementById(i).className = "element comparingElement";
-          
           // found new minimum
           if (numbers[i] < numbers[lowestNumIndex]) {
             if (lowestNumIndex != startIndex) {
@@ -90,20 +91,14 @@ function sortSingleStep(startIndex) {
               document.getElementById(i).className = "element";
             }, speed);
           }
-          
           if (i == numbers.length - 1) {
             swapElements(startIndex, lowestNumIndex);
           }
-        
         }, speed*i);
-      
       })(i); 
-      document.getElementById(i).className = "element";
-      
+      document.getElementById(i).className = "element"; 
     }
   }
-	
-	
 }
 
 function swapElements(startIndex, lowestNumIndex) {
@@ -134,18 +129,120 @@ function swapElements(startIndex, lowestNumIndex) {
       startElement.style.paddingTop = lowestElement.style.paddingTop;
       lowestElement.style.paddingTop = paddingTemp;
       
-    }, 700);
-  } 
-    // reset display
-    setTimeout(function () {
-        document.getElementById(lowestNumIndex).className = "element";
-        if (running) {
-          button.click(); // move to next step
-        } else {
-          startButton.innerText = "Resume Sort";
-          document.getElementById(startIndex).className = "element minimumElement";
-        }
-      }, 1400);
+    }, speed * 3);
+  }
+  // reset display
+  setTimeout(function () {
+    if (startIndex == lowestNumIndex) {
+      document.getElementById(startIndex).className = "element minimumElement";
+    } else {
+      document.getElementById(lowestNumIndex).className = "element";
+    }
+
+    if (running) {
+      // more to sort
+      if (sortStep < numbers.length - 1) {
+        button.click(); // move to next step
+      } 
+      // reached the end
+      else {
+        running = false;
+        startButton.innerText = "Binary Search";
+        document.getElementById("result").innerText = "Sorted!";
+        document.getElementById("searchNum").style.display = "inline";
+        document.getElementById("searchNum").value = numbers[Math.floor(Math.random() * numbers.length)];
+        document.getElementById(numbers.length - 1).className = "element minimumElement";
+      }
+    } 
+    // user stopped program
+    else {
+      startButton.innerText = "Resume Sort";
+      document.getElementById(startIndex).className = "element minimumElement";
+    }
+  }, speed * 6);
+}
+
+// start search and display result at end
+function startBinarySearch() {
+  // reset element displays
+  for (i = 0; i < numbers.length; i++) {
+    document.getElementById(i).className = "element";
+  }
+  document.getElementById("result").innerText = "Searching...";
+
+  // store numbers
+  searchNum = document.getElementById("searchNum").value;
+  binarySearch(1, searchNum, 0, numbers.length - 1).then(function(result) {
+    // update display
+    if (result != -1 && result != null) {
+      document.getElementById(result).className = "element midElement";
+      document.getElementById("result").innerText = searchNum + " found at index " + result + "!";
+    } else {
+      document.getElementById("result").innerText = searchNum + " not found in list.";
+    }
+  });
+  
+}
+
+// search logic
+function binarySearch(iteration, num, left, right) {
+
+  // promise for waiting for timouts to finish to return
+  var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      // until left/right cross
+      if (left <= right) {
+        let mid;
+        // set elements to display correctly
+        setTimeout(function() {
+          mid = Math.floor(left + ((right - left) / 2));
+          document.getElementById(left).className = "element leftElement";
+          document.getElementById(mid).className = "element midElement";
+          document.getElementById(right).className = "element rightElement";
+        }, speed);
+    
+        // number found
+        setTimeout(function() {
+          if (numbers[mid] == num) {
+            resolve(mid);
+      
+          // number in first half
+          } else if (numbers[mid] > num) {
+            // wait to update display
+            setTimeout(function() {
+              document.getElementById(mid).className = "element wrongElement";
+            }, speed * 3);
+            // wait longer to update display
+            setTimeout(function() {
+              document.getElementById(mid).className = "element";
+              document.getElementById(mid - 1).className = "element rightElement";
+              document.getElementById(right).className = "element";
+              resolve(binarySearch(iteration + 1, num, left, mid - 1));
+            }, speed * 9);
+          } 
+          // number in second half
+          else if (numbers[mid] < num) {
+            // wait to update display
+            setTimeout(function() {
+              document.getElementById(mid).className = "element wrongElement";
+            }, speed * 3);
+            // wait longer to update display
+            setTimeout(function() {
+              document.getElementById(left).className = "element";
+              document.getElementById(mid).className = "element";
+              document.getElementById(mid + 1).className = "element leftElement";
+              resolve(binarySearch(iteration + 1, num, mid + 1, right));
+            }, speed * 9);
+          }
+        }, speed * 6);
+      } 
+      // number not found
+      else {
+        resolve(-1);
+      }
+    }, speed);
+  })
+  return promise;
 }
 
 
